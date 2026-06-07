0e883d5e56a74a5e8ac2fbb7ff9b016ef98a8fba
taronghazaryan <119658731+taronghazaryan@users.noreply.github.com>
2026-06-07 15:15:10 +0400

    Fix authorization header formatting

--- src/app/games/[slug]/page.js
+++ src/app/games/[slug]/page.js
@@ -5,7 +5,7 @@ import ReviewsSwiper from "../../components/GameView/ReviewsSwiper";
 import axios from "axios";
 
 const API_HEADERS = {
-  Authorization: 'dQbLtO9jVjPXZYVWr!xbUc%O8k%yNgOQahOoXZjWivr5FFoN!BZU*619Y*w$$XhLg',
+  Authorization: 'dQbLtO9jVjPXZYVWr!xbUc%O8k%yNgOQahOoXZjWivr5FFoN!BZU*619Y*w$XhLg',
 };
 const API_URL = "https://psgamezz.ru/api/games/";
 
